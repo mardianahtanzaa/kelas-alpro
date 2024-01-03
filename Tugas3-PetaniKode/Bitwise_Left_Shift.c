9a203b17bfb6f9348fa75bc2b578207a59ab4b71
mardianahtanzaa <147507121+mardianahtanzaa@users.noreply.github.com>
2024-01-03 08:56:02 +0700

    Add files via upload

---
+++ Tugas3-PetaniKode/Bitwise_Left_Shift.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(){
+    int a = 6;
+
+    printf("a << 1 = %d\n", a << 1);
+    printf("a >> 1 = %d\n", a >> 1);
+}
---
+++ Tugas3-PetaniKode/bitwise_not.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(){
+    int a = 4;
+
+    printf("~a = %d\n", ~a);
+}

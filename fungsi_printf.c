629e58c0f3c6fe1010b3f72e31b9d68f01dc4d9c
mardianahtanzaa <147507121+mardianahtanzaa@users.noreply.github.com>
2024-01-03 09:21:02 +0700

    Delete fungsi_printf.c

--- fungsi_printf.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Halo, ayo kenalan lebih lanjut \n");
-    printf("Nama saya adalah %s\n", "Nana");
-    printf("Usia saya adalah %d\n", 19);
-    return 0;
-}

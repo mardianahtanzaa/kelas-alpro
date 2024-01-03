8796ff2cbde5cce6b7aef91850f8812224ebbcc3
mardianahtanzaa <147507121+mardianahtanzaa@users.noreply.github.com>
2024-01-03 08:28:41 +0700

    Create define.c

---
+++ Tugas3-PetaniKode/define.c
@@ -0,0 +1,12 @@
+// menggunakan define 
+
+#include <stdio.h>
+#define SEPULUH  10 
+#define VERSI  3.0 
+#define JENIS_KELAMIN 'P' 
+
+void main(){
+    printf("isi konstanta SEPULUH adalah %d\n", SEPULUH);
+    printf("isi konstanta VERSI adalah %.2f\n", VERSI);
+    printf("isi konstanta JENIS_KELAMIN adalah %c\n", JENIS_KELAMIN);
+}

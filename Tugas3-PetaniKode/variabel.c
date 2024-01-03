27de05fccc93d67a7c563ed7a6fbb310d828da0f
mardianahtanzaa <147507121+mardianahtanzaa@users.noreply.github.com>
2024-01-03 08:24:51 +0700

    Create variabel.c

---
+++ Tugas3-PetaniKode/variabel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  
+    int weight;
+    int height = 158;
+
+    // mengisi nilai ke variabel
+    weight = 42;
+
+  printf("Berat badan saya adalah %d kg dan tinggi %d cm", weight, height);
+
+    return 0;
+}

bbd216874da70701056b5f8b14744c1b82704d3e
mardianahtanzaa <147507121+mardianahtanzaa@users.noreply.github.com>
2024-01-03 09:30:44 +0700

    Create sequential_search.c

---
+++ Tugas10-Searching/sequential_search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define SIZE 100
+
+size_t linearSearch(const int array[], int key, size_t size);
+
+int main(void){
+    int a[SIZE];
+    for(size_t x=0; x<SIZE; x++){
+        a[x]=2*x;
+    }
+
+    printf("Tuliskan Bilangan yang Dicari: ");
+    int searchKey;
+    scanf("%d", &searchKey);
+
+    size_t index=linearSearch(a, searchKey, SIZE);
+
+    if(index!=-1){
+        printf("Menemukan Nilai pada Index %d\n", index);
+    }else{
+        puts("Nilai Tidak Ditemukan");
+    }
+}
+
+size_t linearSearch(const int array[], int key, size_t size){
+    for(size_t n=0; n<size; n++){
+        if(array[n]==key){
+            return n;
+        }
+    }
+    return -1;
+}

efe8e17f76f1701f23332e9e3fea0eeff86be531
mardianahtanzaa <147507121+mardianahtanzaa@users.noreply.github.com>
2024-01-03 08:19:16 +0700

    Create fungsi_printf.c

---
+++ Tugas3-PetaniKode/fungsi_printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Halo, ayo kenalan lebih lanjut \n");
+    printf("Nama saya adalah %s\n", "Nana");
+    printf("Usia saya adalah %d\n", 19);
+    return 0;
+}
